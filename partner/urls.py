a3d6382dd9e68d0b255755ad1400d046056ad966
Minsoo Kwak <marcosdeseul@gmail.com>
2017-01-12 17:19:49 +0800

    Add menu deletion feature

--- partner/urls.py
+++ partner/urls.py
@@ -3,7 +3,7 @@ from .views import (
     index,
     edit_info,
     signup, login, logout, # auth
-    menu, menu_add, menu_detail, menu_edit,
+    menu, menu_add, menu_detail, menu_edit, menu_delete,
 )
 
 urlpatterns = [
@@ -16,4 +16,5 @@ urlpatterns = [
     url(r'^menu/add/$', menu_add, name="menu_add"),
     url(r'^menu/(?P<menu_id>\d+)/$', menu_detail, name="menu_detail"),
     url(r'^menu/(?P<menu_id>\d+)/edit/$', menu_edit, name="menu_edit"),
+    url(r'^menu/(?P<menu_id>\d+)/delete/$', menu_delete, name="menu_delete"),
 ]
